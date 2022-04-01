5e64c77d0d47d1f2d0bca756ece7018ac8a4c91e
Agatha Harkness <caitlinmarie.clifford@gmail.com>
2022-04-01 19:27:18 -0400

    bugs

--- Develop/script.js
+++ Develop/script.js
@@ -1,38 +1,57 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-window.alert("Welcome to Password Generator! Please click the 'Generate Password' button to begin.");
+window.confirm("Welcome to Password Generator!");
+if (confirm) {
+  passwordLength();
+}
 
-function generatePassword() {
-  var promptLength = window.prompt("Please choose a password length ranging from 8 to 128 characters.");
+function passwordLength() {
 
-  if (promptLength === "" || promptLength === null) {
-    window.alert("You must choose a number ranging from 8 to 128. Please try again.");
+  // Set parameters for password length //
+    var promptLength = window.prompt("Please choose a password length ranging from 8 to 128 characters.");
 
-    return generatePassword();
+    if (promptLength === "" || promptLength === null) {
+      window.alert("You must choose a number ranging from 8 to 128. Please try again.");
+
+      return passwordLength();
+    }
+
+    if (promptLength < 8) {
+      window.alert("You can choose a minimum length of 8. Please try again.");
+
+      return passwordLength();
+    }
+
+    if (promptLength > 128) {
+      window.alert("You can choose a maximum length of 128. Please try again.");
+
+      return passwordLength();
+    }
+
+    if (isNaN(promptLength)) {
+      window.alert("You must choose a NUMBER ranging from 8 to 128. Please try again.");
+
+      return passwordLength();
+    }
+
+    if (promptLength >= 8 && promptLength <= 128) {
+      window.alert("You have chosen a length of " + promptLength + " .");
+    }
+    console.log(promptLength);
+  
+  
+
+  // set parameters for character types //
+  var optionLowercase = function() { 
+    window.prompt("Would you like to use lowercase letters in your password?");
+  
+    if (optionLowercase === "" || optionLowercase === null) {
+      window.alert("You must choose a valid answer. Please try again.");
+      return optionLowercase()
+    }
   }
 
-  if (promptLength < 8) {
-    window.alert("You must choose a minimum length of 8. Please try again.");
-
-    return generatePassword();
-  }
-
-  if (promptLength > 128) {
-    window.alert("You must choose a maximum length of 128. Please try again.");
-
-    return generatePassword();
-  }
-
-  if (isNaN(promptLength)) {
-    window.alert("You must choose a NUMBER ranging from 8 to 128. Please try again.");
-
-    return generatePassword();
-  }
-
-  if (promptLength >= 8 && promptLength <= 128) {
-    window.alert("You have chosen a length of " + promptLength + " .");
-  }
 };
 
 // Write password to the #password input
@@ -43,6 +62,8 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+//function generatePassword()
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
