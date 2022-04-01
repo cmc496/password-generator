06d2a515ad9b179b7392912eecf436430ee7e356
Agatha Harkness <caitlinmarie.clifford@gmail.com>
2022-04-01 18:30:24 -0400

    promptLength parameters set

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,39 @@
-// Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+window.alert("Welcome to Password Generator! Please click the 'Generate Password' button to begin.");
+
+function generatePassword() {
+  var promptLength = window.prompt("Please choose a password length ranging from 8 to 128 characters.");
+
+  if (promptLength === "" || promptLength === null) {
+    window.alert("You must choose a number ranging from 8 to 128. Please try again.");
+
+    return generatePassword();
+  }
+
+  if (promptLength < 8) {
+    window.alert("You must choose a minimum length of 8. Please try again.");
+
+    return generatePassword();
+  }
+
+  if (promptLength > 128) {
+    window.alert("You must choose a maximum length of 128. Please try again.");
+
+    return generatePassword();
+  }
+
+  if (isNaN(promptLength)) {
+    window.alert("You must choose a NUMBER ranging from 8 to 128. Please try again.");
+
+    return generatePassword();
+  }
+
+  if (promptLength >= 8 && promptLength <= 128) {
+    window.alert("You have chosen a length of " + promptLength + " .");
+  }
+};
 
 // Write password to the #password input
 function writePassword() {
