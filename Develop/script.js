e9a2859378d6d1cbe192f195ae0fd6410e166c15
Agatha Harkness <caitlinmarie.clifford@gmail.com>
2022-04-02 14:45:17 -0400

    password now generated

--- Develop/script.js
+++ Develop/script.js
@@ -7,7 +7,7 @@ function writePassword() {
   //function generatePassword()
 
   function generatePassword() {
-    debugger;
+    //debugger;
     passwordLength();
     
     
@@ -16,36 +16,38 @@ function writePassword() {
 
   // Set parameters for password length //
 function passwordLength() {
-  var promptLength = window.prompt("Please choose a password length ranging from 8 to 128 characters.");
+  var optionLength = window.prompt("Please choose a password length ranging from 8 to 128 characters.");
 
-  if (promptLength === "" || promptLength === null) {
+  if (optionLength === "" || optionLength === null) {
     window.alert("You must choose a number ranging from 8 to 128. Please try again.");
 
     return passwordLength();
   }
 
-  if (promptLength < 8) {
+  if (optionLength < 8) {
     window.alert("You can choose a minimum length of 8. Please try again.");
 
     return passwordLength();
   }
 
-  if (promptLength > 128) {
+  if (optionLength > 128) {
     window.alert("You can choose a maximum length of 128. Please try again.");
 
     return passwordLength();
   }
 
-  if (isNaN(promptLength)) {
+  if (isNaN(optionLength)) {
     window.alert("You must choose a NUMBER ranging from 8 to 128. Please try again.");
 
     return passwordLength();
   }
 
-  if (promptLength >= 8 && promptLength <= 128) {
-    window.alert("You have chosen a length of " + promptLength + " .");
+  if (optionLength >= 8 && optionLength <= 128) {
+    window.alert("You have chosen a length of " + optionLength + " .");
+    //i = promptLength - 8;
   }
-  console.log(promptLength);
+  console.log(optionLength);
+  //console.log("i = " + i);
 
 
 passwordLowercase();
@@ -56,21 +58,29 @@ passwordLowercase();
 function passwordLowercase() {
   var optionLowercase = window.prompt("Would you like to use lowercase letters in your password? Please respond with 'Yes' or No.'");
     optionLowercase = optionLowercase.toLowerCase();
+    var lowercase = [
+      "",
+      1,
+      2
+    ];
 
   if (optionLowercase === "" || optionLowercase === null) {
     window.alert("You need to provide a valid answer. Please try again.");
     return passwordLowercase();
   }
 
-  if (optionLowercase == "yes") {
+  if (optionLowercase === "yes") {
     var optionLowercase = 1;
+    lowercase[1] = "abcdefghijklmnopqrstuvwxyz";
   }
 
-  if (optionLowercase == "no") {
+  if (optionLowercase === "no") {
     var optionLowercase = 2;
+    lowercase[2] = "";
   }
 
   console.log(optionLowercase);
+
 passwordUppercase();
 
 // uppercase option //
@@ -78,6 +88,11 @@ passwordUppercase();
 function passwordUppercase() {
  var optionUppercase = window.prompt("Would you like to use uppercase letters in your password? Please respond with 'Yes' or No.'");
   optionUppercase = optionUppercase.toLowerCase();
+  var uppercase = [
+    "",
+    1,
+    2
+  ];
 
   if (optionUppercase === "" || optionUppercase === null) {
     window.alert("You need to provide a valid ansewr. Please try again.")
@@ -86,11 +101,14 @@ function passwordUppercase() {
 
   if (optionUppercase == "yes") {
     var optionUppercase = 1;
+    uppercase[1] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   }
 
   if (optionUppercase == "no") {
     var optionUppercase = 2;
+    uppercase[2] = "";
   }
+
   console.log(optionUppercase);
 passwordNumbers();
 
@@ -99,6 +117,11 @@ passwordNumbers();
 function passwordNumbers() {
   var optionNumbers = window.prompt("Would you like to use numbers in your password? Please respond with 'Yes' or No.'");
   optionNumbers = optionNumbers.toLowerCase();
+  var numbers = [
+    "",
+    1,
+    2
+  ];
 
   if (optionNumbers === "" || optionNumbers === null) {
     window.alert("You need to provide a valid answer. Please try again.");
@@ -107,11 +130,14 @@ function passwordNumbers() {
 
   if (optionNumbers == "yes") {
     var optionNumbers = 1;
+    numbers[1] = "1234567890";
   }
 
   if (optionNumbers == "no") {
     var optionNumbers = 2;
+    numbers[2] = "";
   }
+
   console.log(optionNumbers);
 
 passwordSpecial();
@@ -121,6 +147,11 @@ passwordSpecial();
 function passwordSpecial() {
   var optionSpecial = window.prompt("Would you like to use special characters in your password? Please respond with 'Yes' or No.'");
   optionSpecial = optionSpecial.toLowerCase();
+  var special = [
+    "",
+    1,
+    2
+  ];
 
   if (optionSpecial === "" || optionSpecial === null) {
     window.alert("You need to provide a valid answer. Please try again.");
@@ -129,11 +160,14 @@ function passwordSpecial() {
 
   if (optionSpecial == "yes") {
     var optionSpecial = 1;
+    special[1] = "!@#$%^&*()";
   }
 
   if (optionSpecial == "no") {
     var optionSpecial = 2;
+    special[2] = "";
   }
+
   console.log(optionSpecial);
 
   // Make sure user must choose at least one character type //
@@ -145,6 +179,27 @@ function passwordSpecial() {
       window.alert("You must choose at least one character type. Please try again.");
       return passwordLowercase();
     }
+
+    passwordGenerated();
+
+    function passwordGenerated() {
+      //var generatedPassword = (optionLength) => []
+      var length = optionLength;
+      const charset = lowercase[optionLowercase] + uppercase[optionUppercase] + numbers[optionNumbers] + special[optionSpecial];
+      var generatedPassword = "";
+      const charsetLength = charset.length;
+      console.log("charset.length is " + charset.length);
+      //console.log("length is " + optionLength);
+      console.log("character choices: " + charset);
+    debugger;
+      for ( var i = 0; i < length; i++ ) {
+        generatedPassword += charset.charAt(Math.floor(Math.random() * charsetLength));
+      //console.log(generatedPassword);
+      }
+      //return generatedPassword;
+      console.log("generated password is " + generatedPassword);
+    }
+    
   };
 };
 };
